7de63912749a6b9ce6f55466245f7c9511a15fa7
Alex Ipiales <aeipialesc@utn.edu.ec>
2022-01-19 19:53:54 -0500

    chage url https

--- src/helpers/GetGifs.js
+++ src/helpers/GetGifs.js
@@ -2,7 +2,7 @@
 
 export const GetGifs = async (category) => {
     const url =
-`http://api.giphy.com/v1/gifs/search?q=${ encodeURI( category )}&limit=10&api_key=u4B9VSduRU3c3UwUFbHJV0yUouiHa5mM`;
+`https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category )}&limit=10&api_key=u4B9VSduRU3c3UwUFbHJV0yUouiHa5mM`;
     const resp = await fetch(url);
     const { data } = await resp.json();
 
